99527be682694f6c6a0864846852a4bd110c6a4d
eriveltonmessias <erivelton-messias2011@hotmail.com>
2026-07-27 12:08:19 -0300

    fix: serialize Kubernetes secret values

--- deploy/terraform/locals.tf
+++ deploy/terraform/locals.tf
@@ -3,8 +3,8 @@ locals {
     apps_namespace        = data.terraform_remote_state.data_platform.outputs.apps_namespace
     image                 = "${data.terraform_remote_state.infra.outputs.ecr_repository_urls["video-worker-api"]}:${var.image_tag}"
     replicas              = var.replicas
-    s3_access_key         = var.s3_access_key
-    s3_secret_key         = var.s3_secret_key
+    s3_access_key         = jsonencode(var.s3_access_key)
+    s3_secret_key         = jsonencode(var.s3_secret_key)
     s3_endpoint           = "https://s3.${var.aws_region}.amazonaws.com"
     s3_input_bucket_name  = data.terraform_remote_state.data_platform.outputs.s3_input_bucket
     s3_output_bucket_name = data.terraform_remote_state.data_platform.outputs.s3_output_bucket
